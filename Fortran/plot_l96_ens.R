7a12fa69e2e040e32b2167e124efa81161083eaf
eno <>
2024-08-14 02:09:33 +0000

    fix abline
    FossilOrigin-Name: 6ecb9cd431bbb6be069727685ecb10ae7d5eb4cebd0d3630c01d6f321845225d

--- Fortran/plot_l96_ens.R
+++ Fortran/plot_l96_ens.R
@@ -13,7 +13,7 @@ plot(1:nt, e[,1], lwd=3,
      main=title, xlab="time", ylab="L2", type="l", ylim=c(0, 3),
      cex.lab=2, cex.axis=2, cex.main=3
 )
-abline(h=1.0, lt=2)
+abline(h=sqrt(r), lt=2)
 lines(1:nt, e[,3], lwd=3, col="blue")
 lines(1:nt, e[,2], lwd=3, col="red")
 legend("topleft", cex=2, legend=c("mean", "stddev", "member"),
--- R/plot_l96_var.R
+++ R/plot_l96_var.R
@@ -6,7 +6,7 @@ close(con)
 title <- paste("4DVar", "step=", a)
 plot(seq(1, nt, by=nw), l2, type="l", lwd=2,
      main=title, xlab="time", ylab="L2", ylim=c(0, 3))
-abline(h=1, lt=2)
+abline(h=sqrt(r), lt=2)
 abline(h=mean(l2), lt=3)
 legend("topleft", legend=c(
   "analysis", paste("mean", format(mean(l2), digits=3)), "observation"),
