6b04244fa770958b758cf5a114b589d89129934e
eno <>
2024-08-15 04:49:35 +0000

    add ode.R
    FossilOrigin-Name: b9e0ef853675bb545cfc7cc9e1c00bd0ef06bf944d05d4ceb38f221b22e5cf3e

---
+++ R/ode.R
@@ -0,0 +1,28 @@
+source("rk4.R")
+
+ode.fom <- function(f, x, nstep, dt, ...) {
+  t <- 0
+  for (i in 1:nstep) {
+    x <- rk4(f, t, x, dt, ...)
+    t <- t + dt
+  }
+  x
+}
+
+ode.tlm <- function(f, df, x, dx, nstep, dt, ...) {
+  t <- 0
+  for (i in 1:nstep) {
+    dx <- rk4.tl(f, df, t, x, dx, dt, ...)
+    t <- t + dt
+  }
+  dx
+}
+
+ode.adm <- function(f, fa, x, xa, nstep, dt, ...) {
+  t <- nstep
+  for (i in nstep:1){
+    xa <- rk4.ad(f, fa, t, x, xa, dt, ...)
+    t <- t - dt
+  }
+  xa
+}
