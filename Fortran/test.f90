469812d8b961cb0c1f446c5f6f9972c758898bca
eno <>
2024-08-17 02:07:53 +0000

    remove io_module
    FossilOrigin-Name: e8157bd26579d0734117b80544d8f0ba3fe2c325a9e2b6a36d40272334a65887

--- Fortran/test.f90
+++ Fortran/test.f90
@@ -2,7 +2,6 @@ program test
   use random_module, only: random_test
   use l63_module, only: l63_test
   use l96_module, only: l96_test
-  use io_module, only: io_test
   implicit none
 
   print *, "random:"
@@ -11,7 +10,5 @@ program test
   call l63_test(1.0d-5)
   print *, "l96:"
   call l96_test(1.0d-5)
-  print *, "io:"
-  call io_test()
 
 end program test
